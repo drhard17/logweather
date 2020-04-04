78396b3c0e5923650b5b5c6fb0c32d3c6fdef686
drhard17 <maslak84@gmail.com>
2020-04-05 00:36:22 +0300

    Incoming IP logging

--- src/web-server.js
+++ src/web-server.js
@@ -9,11 +9,13 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
 	const { pathname, query } = url.parse(req.url, true);
+	const ip = res.socket.remoteAddress
+	console.log(`${ip} connected`)
 	if (query.temp) {
 		logw.toCSV('STREET', [query.temp])
+		console.log(`${ip} added temperature`)
 	}
 	htmlFormer.addTemp((data) => {
-		//res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/html');
 		res.write(data);
 		res.end();
