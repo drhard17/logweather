eaf1a9935049866881a3b0c39eeb40b1fc7a509b
drhard17 <maslak84@gmail.com>
2020-05-25 22:32:27 +0300

    test site parser added

--- src/Crawler/temp-parsers/weathercom-temp-parser.js
+++ src/Crawler/temp-parsers/weathercom-temp-parser.js
@@ -1,5 +1,6 @@
 const { JSDOM } = require('jsdom');
 module.exports = {
+	name: 'WEATHERCOM',
 	opts: {
 		hostname: 'weather.com',
 		path: '/ru-RU/weather/tenday/l/5dee46352b93411b346eb161aaf601f33320283181d1f8aa42afe10f700e00ff',
@@ -9,27 +10,31 @@ module.exports = {
 			"User-Agent": "PostmanRuntime/7.22.0"
 		}
 	},
-	name: 'WEATHERCOM',
 	parseFunc: function(page) {
 		let start = page.indexOf('<style')
 		let end = page.indexOf('</style>')
 		page = page.slice(0, start) + page.slice(-1 * (page.length - end))
-				
-		const dom = new JSDOM(page, { runScripts: "dangerously", pretendToBeVisual: true });
+
+		const dom = new JSDOM(page);
 		let results = []
 		
-		let currTemp = dom.window.document
-			.querySelector('span.styles__temperature__1VbnH')
-						
-		//console.log('Current: ' + currTemp.outerHTML)
-		//console.log(this.opts.hostname)
-		
-		let tds = dom.window.document
-			.querySelector('tbody')
-			.querySelectorAll('td.temp')
-		for (let td of tds) {
-			let temp = td.querySelector('span').innerHTML
-			results.push(parseInt(temp, 10))
+		try {
+			let tds = dom.window.document
+				.querySelector('tbody')
+				.querySelectorAll('td.temp')
+			for (let td of tds) {
+				let temp = td.querySelector('span').innerHTML
+				results.push(parseInt(temp, 10))
+			}
+		} catch (error) {
+			const temps = Array.from(dom.window.document
+				.querySelector('div._-_-components-src-organism-DailyForecast-DailyForecast--DisclosureList--nosQS')
+				.querySelectorAll('details')
+			).filter(str => str.matches('details[data-track-string="detailsExpand"]'))
+			.map(str => str.querySelector('span').innerHTML)
+			.map(temp => parseInt(temp, 10))
+			results = [NaN].concat(temps)
+			console.log('WEATHERCOM test site')
 		}
 		return results;
 	}
