376261a3cdabcd89ff9a92f5775406d72575bede
drhard17 <maslak84@gmail.com>
2020-07-25 22:21:21 +0300

    logging fixed

--- src/Backend/db-storage.js
+++ src/Backend/db-storage.js
@@ -44,7 +44,8 @@ module.exports = {
         const locId = tempRecords.map(tempRecord => tempRecord.locId)[0] //for log
         db.run(sql, (err) => {
             if (err) return console.log(err);
-            console.log(`${locId} - ${tempRecords.length} temprecords added to DB`)
+            const date = new Date().toLocaleTimeString()
+            console.log(`${date} - locId ${locId} - ${tempRecords.length} temprecords added`)
         })
 
         db.close((err) => {
@@ -55,15 +56,14 @@ module.exports = {
     getLastTemp: function(serviceName, locId, cb) {
         const db = getLogweatherDb()
         const sql = `SELECT MAX(datetime) AS datetime, service_id, location_id, temp FROM forecasts
-                        WHERE depth = 0 
-                        AND service_id = (SELECT id FROM services WHERE name = '${serviceName}')
-                        AND location_id = ${parseInt(locId, 10)}
+                        WHERE service_id = (SELECT id FROM services WHERE name = (?))
+                        AND location_id = (?)
+                        AND depth = 0
                     `
-        db.all(sql, (err, rows) => {
-            console.log(serviceName, locId, err);
+        const locationId = parseInt(locId, 10)                     
+        db.all(sql, [serviceName, locationId], (err, rows) => {
             if (err) { return cb(err) }
             if (!rows.length) { return cb(null, null) }
-            
             const temp = rows[0].temp
             cb(null, temp)
         })
@@ -77,7 +77,7 @@ module.exports = {
     getTempData: function (req, cb) {
         const firstDay = getDbDate(req.firstDay)
         const lastDay = getDbDate(req.lastDay)
-        const hour = req.hour
+        const hour = String(req.hour)
         const locId = req.locId
         
         const db = getLogweatherDb()
@@ -85,12 +85,11 @@ module.exports = {
             SELECT datetime AS timeStamp, services.name AS serviceName, depth, temp
             FROM forecasts
             INNER JOIN services ON forecasts.service_id = services.id
-            WHERE datetime BETWEEN ${firstDay} AND ${lastDay}
-                AND location_id = ${locId}
-                AND (SELECT strftime('%H', datetime(datetime, 'unixepoch'))) = '${hour}'
+            WHERE datetime BETWEEN (?) AND (?)
+                AND location_id = (?)
+                AND (SELECT strftime('%H', datetime(datetime, 'unixepoch'))) = (?)
         `
-        
-        db.all(sql, (err, rows) => {
+        db.all(sql, [firstDay, lastDay, locId, hour], (err, rows) => {
             if (err) { return cb(err) }
             const result = rows.map(row => {
                 return {
