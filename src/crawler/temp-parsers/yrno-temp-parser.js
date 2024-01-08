e09b49c2824a7dd27267e9046d94f2c74dd2de14
drhard17 <maslak84@gmail.com>
2024-01-08 23:22:26 +0300

    fixed parsing bug

--- src/crawler/temp-parsers/yrno-temp-parser.js
+++ src/crawler/temp-parsers/yrno-temp-parser.js
@@ -9,14 +9,11 @@ module.exports = {
 		const { document } = new JSDOM(page).window
 
 		const dayToday = new Date().getDate()
-		// const currentTemp = parseInt(
-		// 	document
-		// 		.querySelector('div.now-hero__next-hour-temperature-text > span')
-		// 		.textContent
-		// , 10)
+
 		const temps = Array.from(
 			document.querySelectorAll('ol.daily-weather-list__intervals > li')
-		).map(li => {
+		).filter(li => li.querySelector('span.min-max-temperature') !== null)
+		.map(li => {
 			const temp = li
 				.querySelector('span.min-max-temperature')
 				.textContent
